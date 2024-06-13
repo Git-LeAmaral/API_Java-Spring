2e340fb6c29a24ad11c9a41af69c26e4bdfee8c1
Leandro Amaral <lramaral02m@gmail.com>
2024-06-13 15:07:45 -0300

    Subindo projeto

---
+++ api/src/main/java/br/com/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,36 @@
+package br.com.med.voll.api.controller;
+
+import br.com.med.voll.api.medico.DadosCadastroMedico;
+import br.com.med.voll.api.medico.DadosListagemMedico;
+import br.com.med.voll.api.medico.Medico;
+import br.com.med.voll.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+
+        repository.save(new Medico(dados));
+
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+}
